067e65d5e52b740f96812a91c5df1c167ba5271b
Jiří Gracík <email@jirigracik.cz>
2021-06-04 11:24:37 +0200

    [CI] Build application and run cs, phpunit and phpstan

---
+++ ecs.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    // A. standalone rule
+    $services = $containerConfigurator->services();
+    $services->set(ArraySyntaxFixer::class)
+        ->call('configure', [[
+            'syntax' => 'short',
+        ]]);
+
+    // B. full sets
+    $containerConfigurator->import(SetList::PSR_12);
+};
+
